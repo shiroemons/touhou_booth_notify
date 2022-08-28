1b128c6c0365a13d3909ff75ad0532a9ca5f3488
shiroemons <shiroemons@gmail.com>
2022-08-28 16:10:19 +0900

    Implementation of DB insert, update

--- go.mod
+++ go.mod
@@ -2,7 +2,13 @@ module github.com/shiroemons/touhou_booth_notify
 
 go 1.18
 
-require github.com/gocolly/colly v1.2.0
+require (
+	github.com/gocolly/colly v1.2.0
+	github.com/shopspring/decimal v1.3.1
+	github.com/uptrace/bun v1.1.7
+	github.com/uptrace/bun/dialect/pgdialect v1.1.7
+	github.com/uptrace/bun/driver/pgdriver v1.1.7
+)
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
@@ -10,17 +16,22 @@ require (
 	github.com/antchfx/htmlquery v1.2.5 // indirect
 	github.com/antchfx/xmlquery v1.3.12 // indirect
 	github.com/antchfx/xpath v1.2.1 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
-	gopkg.in/yaml.v2 v2.2.5 // indirect
+	mellium.im/sasl v0.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,24 +1,81 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"log"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly"
+	"github.com/shopspring/decimal"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
 )
 
 type Item struct {
-	Category string
-	Name     string
-	ShopName string
-	Price    string
-	URL      string
-	ImageURL string
+	bun.BaseModel `bun:"table:items,alias:i"`
+
+	ID        int64     `bun:"id,pk,autoincrement"`
+	Name      string    `bun:"name,notnull"`
+	Category  string    `bun:"category,notnull,default:''"`
+	Price     string    `bun:"price,type:numeric,notnull"`
+	URL       string    `bun:"url,notnull"`
+	ImageURL  string    `bun:"image_url,notnull"`
+	ShopName  string    `bun:"-"`
+	CreatedAt time.Time `bun:"created_at,notnull,default:current_timestamp"`
+	UpdatedAt time.Time `bun:"updated_at,notnull,default:current_timestamp"`
+}
+
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("Warning: %s environment variable not set.", k)
+	}
+	return v
 }
 
 func main() {
-	baseURL := "https://booth.pm/ja/browse/%E9%9F%B3%E6%A5%BD?in_stock=true&new_arrival=true&q=%E6%9D%B1%E6%96%B9Project&sort=new&type=digital"
+	ctx := context.Background()
 
+	dsn := mustGetenv("DSN")
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	db := bun.NewDB(sqldb, pgdialect.New())
+
+	var v string
+	if err := db.NewSelect().ColumnExpr("version()").Scan(ctx, &v); err != nil {
+		panic(err)
+	}
+
+	items, err := getItems()
+	if err != nil {
+		log.Fatalf("getItems error: %s", err)
+	}
+
+	for i := len(items) - 1; i >= 0; i-- {
+		run(ctx, db, items[i])
+	}
+}
+
+var _ bun.BeforeAppendModelHook = (*Item)(nil)
+
+func (i *Item) BeforeAppendModel(ctx context.Context, query bun.Query) error {
+	switch query.(type) {
+	case *bun.InsertQuery:
+		now := time.Now()
+		i.CreatedAt = now
+		i.UpdatedAt = now
+	case *bun.UpdateQuery:
+		i.UpdatedAt = time.Now()
+	}
+	return nil
+}
+
+func getItems() ([]*Item, error) {
+	baseURL := "https://booth.pm/ja/browse/%E9%9F%B3%E6%A5%BD?in_stock=true&new_arrival=true&q=%E6%9D%B1%E6%96%B9Project&sort=new&type=digital"
 	c := colly.NewCollector()
 
 	var items []*Item
@@ -26,7 +83,7 @@ func main() {
 		category := e.DOM.Find("div.item-card__category").Text()
 		name := e.DOM.Find("div.item-card__title").Text()
 		shopName := e.DOM.Find("div.item-card__shop-name").Text()
-		price := e.Attr("data-product-price")
+		price := e.Attr("data-product-price") + ".0"
 		url, _ := e.DOM.Find("div.item-card__title a").Attr("href")
 		imageURL, _ := e.DOM.Find("div img").Attr("src")
 
@@ -45,10 +102,22 @@ func main() {
 		items = append(items, item)
 	})
 
-	c.Visit(baseURL)
+	err := c.Visit(baseURL)
+	if err != nil {
+		return nil, err
+	}
 
-	for i := len(items) - 1; i >= 0; i-- {
-		item := items[i]
+	return items, nil
+}
+
+func run(ctx context.Context, db *bun.DB, item *Item) {
+	dbItem := itemFindByURL(ctx, db, item.URL)
+
+	if dbItem.ID == 0 {
+		if err := insert(ctx, db, item); err != nil {
+			return
+		}
+
 		tweet := fmt.Sprintf("【🆕新着情報🆕】\n\n%s\n%s\n%s円\n\n%s\n%s\n\n#booth_pm #東方デジタル音楽\n#東方Project #東方楽曲 #東方アレンジ",
 			item.Category,
 			item.Name,
@@ -57,5 +126,47 @@ func main() {
 			item.ShopName,
 		)
 		fmt.Println(tweet)
+	} else if item.Price != dbItem.Price {
+		oldPrice := decimal.RequireFromString(dbItem.Price)
+		newPrice := decimal.RequireFromString(item.Price)
+		dbItem.Price = item.Price
+		if err := update(ctx, db, dbItem); err != nil {
+			return
+		}
+
+		tweet := fmt.Sprintf("【🆙更新情報🆙】\n\n%s\n%s\n%s円 -> %s円\n\n%s\n%s\n\n#booth_pm #東方デジタル音楽\n#東方Project #東方楽曲 #東方アレンジ",
+			item.Category,
+			item.Name,
+			oldPrice,
+			newPrice,
+			item.URL,
+			item.ShopName,
+		)
+		fmt.Println(tweet)
 	}
+}
+
+func itemFindByURL(ctx context.Context, db *bun.DB, url string) *Item {
+	dbItem := new(Item)
+	_ = db.NewSelect().Model(dbItem).Where("url = ?", url).Scan(ctx)
+
+	return dbItem
+}
+
+func insert(ctx context.Context, db *bun.DB, item *Item) error {
+	_, err := db.NewInsert().Model(item).Exec(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func update(ctx context.Context, db *bun.DB, item *Item) error {
+	_, err := db.NewUpdate().Model(item).WherePK().Exec(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
 }
